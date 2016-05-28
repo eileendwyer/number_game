e68fb0aaab8112a24aa0f5859078420bf621fba7
Eileen Dwyer <eolson@smalltalku.com>
2016-05-27 22:19:10 -0400

    final number_game homework

--- number_game.py
+++ number_game.py
@@ -1,29 +1,30 @@
 import random
 def number_game():
     random_num = random.randint(1,100)
-    count = 5
+    count = 0
 
-    while True:
+    for count in range(1,6):
         try:
             guess = int(input("Guess a number between 1 and 100:"))
         except ValueError:
                 print(" {} isn't a number ! Guess again." .format(guess))
         if  guess > random_num:
             print(" {} is too high.".format(guess))
-            count -= 1
         elif guess < random_num:
             print(" {} is too low." .format(guess))
-            count -= 1
-        elif guess == random_num:
-            print("You guessed {}, that's my number !".format(random_num))
-            print(play_again)
         else:
-            print("You didn't guess it. My number was {}.".format(random_num))
-            play_again = input("Do you want to play again? y/n").lower()
-            if play_again == 'y':
-                number_game()
-            else:
-                print("Goodbye!")
+            break
+    if guess == random_num:
+        print("You guessed {}, that's my number !".format(random_num))
+        number_game()
+    else:
+        print("You didn't guess it. My number was {}.".format(random_num))
+        play_again = input("Do you want to play again? y/n").lower()
+        if play_again == 'y':
+            number_game()
+        else:
+            print("Goodbye!")
+
 number_game()
 
 
